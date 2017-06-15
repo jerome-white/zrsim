531854bd4ef5c68d93113c7a30d63918bbcb9974
Jerome White <jerome@thewex.net>
2017-06-15 07:41:34 +0400

    Logging.

--- Manager.java
+++ Manager.java
@@ -1,9 +1,16 @@
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
 public class Manager {
+    private final static Logger LOGGER =
+        Logger.getLogger(Manager.class.getName());
+
     public static void main(String[] args) {
+        LOGGER.setLevel(Level.INFO);
+
         File directory = new File(args[0]);
         int min_ngrams = Integer.parseInt(args[1]);
         int max_ngrams = Integer.parseInt(args[2]);
@@ -13,6 +20,8 @@ public class Manager {
         /*
          * add the documents to the suffix tree
          */
+        LOGGER.info("Adding terms");
+
         int pool = Runtime.getRuntime().availableProcessors();
         ExecutorService es = Executors.newFixedThreadPool(pool);
         for (File document : directory.listFiles()) {
@@ -24,11 +33,15 @@ public class Manager {
         /*
          * Prune the tree by marking entries that are redundant
          */
+        LOGGER.info("Term selection");
+
         root.getChildren().forEachValue(1, v -> v.markRedundants());
 
         /*
-         * Generate terms
+         * Dump the tree to disk
          */
+        LOGGER.info("Terms to disk");
+
         root.getChildren().forEach(1, (k, v) -> {
                 v.accept(new SuffixTreeVisitor(min_ngrams, 2, false));
             });
