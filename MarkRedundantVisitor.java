66e5d4c29b92b8ce151536e64a2d24d72c6f8a58
Jerome White <jerome@thewex.net>
2017-06-15 12:25:29 +0400

    Construction refactor to make consistent with OutputVisitor.

---
+++ MarkRedundantVisitor.java
@@ -0,0 +1,47 @@
+import java.util.NoSuchElementException;
+
+public class MarkRedundantVisitor implements SuffixTreeVisitor {
+    private String ngram;
+    private SuffixTree root;
+    private StringWindow stringWindow;
+
+    public MarkRedundantVisitor(String ngram, SuffixTree root) {
+	this.ngram = ngram;
+        this.root = root;
+
+        stringWindow = new StringWindow(ngram, ngram.length() - 1);
+    }
+
+    public SuffixTreeVisitor spawn(Character gram) {
+        return new MarkRedundantVisitor(ngram + gram, root);
+    }
+
+    public void visit(SuffixTree node) {
+        for (String partial : stringWindow) {
+            try {
+                SuffixTree current = root.find(partial);
+                if (current.isRedundant()) {
+                    continue;
+                }
+
+                int overlap = 0;
+                int epsilon = ngram.length() - partial.length();
+
+                for (Location theirs : current.getLocations()) {
+                    for (Location ours : node.getLocations()) {
+                        if (ours.contains(theirs, epsilon)) {
+                            overlap++;
+                            break;
+                        }
+                    }
+                }
+
+                if (overlap == current.getLocations().size()) {
+                    current.markRedundant();
+                }
+            }
+            catch (NoSuchElementException error) {}
+            catch (IllegalArgumentException error) {}
+        }
+    }
+}
