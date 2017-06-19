0ab200daabf206b4bcf5a1ce94f25dd5009e0c07
Jerome White <jerome@thewex.net>
2017-06-19 23:28:11 +0400

    'shutdown' doesn't wait for executor pool termination!
    Suffix tree output is no longer assumed to be thread safe: threads
    first write to temporary files, then the main thread concatenates
    them.

--- src/test/system/Manager.java
+++ src/test/system/Manager.java
@@ -2,12 +2,20 @@ package test.system;
 
 import java.io.File;
 import java.io.PrintStream;
+import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.UncheckedIOException;
 import java.io.FileNotFoundException;
+import java.nio.channels.FileChannel;
+import java.lang.Long;
+import java.lang.IllegalStateException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ConcurrentHashMap;
 
 import tree.SuffixTree;
 import tree.DocumentParser;
@@ -26,6 +34,7 @@ public class Manager {
         int min_ngrams = Integer.parseInt(args[1]);
         int max_ngrams = Integer.parseInt(args[2]);
         File output = new File(args[3]);
+        File tmpdir = (args.length > 4) ? new File(args[4]) : null;
 
         SuffixTree root = new SuffixTree(min_ngrams);
 
@@ -36,11 +45,19 @@ public class Manager {
 
         int pool = Runtime.getRuntime().availableProcessors();
         ExecutorService es = Executors.newFixedThreadPool(pool);
+
         for (File document : directory.listFiles()) {
             assert !document.isDirectory();
             es.execute(new DocumentParser(root, document, max_ngrams));
         }
         es.shutdown();
+
+        try {
+            es.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        }
+        catch (InterruptedException ex) {
+            throw new IllegalStateException(ex);
+        }
 
         /*
          * Prune the tree by marking entries that are redundant
@@ -48,7 +65,7 @@ public class Manager {
         LOGGER.info("Term selection");
 
         root.getChildren().forEach(1, (k, v) -> {
-                v.accept(new MarkRedundantVisitor(k.toString(), root));
+                v.accept(new MarkRedundantVisitor(k, root));
             });
 
         /*
@@ -56,12 +73,45 @@ public class Manager {
          */
         LOGGER.info("Terms to disk");
 
-        try (PrintStream out = new PrintStream(new FileOutputStream(output))) {
-            root.getChildren().forEach(1, (k, v) -> {
-                    v.accept(new OutputVisitor(k.toString(), 2, false, out));
-                });
+        ConcurrentHashMap<String, File> fragments =
+            new ConcurrentHashMap<String, File>();
+        root.getChildren().forEach(1, (k, v) -> {
+                try {
+                    File tmpfile = (tmpdir == null) ?
+                        File.createTempFile(k, "") :
+                        File.createTempFile(k, "", tmpdir);
+                    assert !fragments.containsKey(k);
+                    File previous = fragments.put(k, tmpfile);
+                }
+                catch (IOException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+            });
+
+        root.getChildren().forEach(1, (k, v) -> {
+                File file = fragments.get(k);
+                try (PrintStream out =
+                     new PrintStream(new FileOutputStream(file), true)) {
+                    v.accept(new OutputVisitor(k, 2, false, out));
+                }
+                catch (FileNotFoundException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+            });
+
+        LOGGER.info("Disk consolidation");
+
+        try (FileChannel dest = new FileOutputStream(output).getChannel()) {
+            for (File in : fragments.values()) {
+                try (FileChannel src = new FileInputStream(in).getChannel()) {
+                    dest.transferFrom(src, dest.size(), src.size());
+                }
+                in.delete();
+            }
         }
-        catch (FileNotFoundException error) {}
+        catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
 
         LOGGER.info("Complete");
     }
