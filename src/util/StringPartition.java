3fe96f8e7c191380d1fcc5abcae9c8496e9649d9
Jerome White <jerome@thewex.net>
2017-06-19 07:25:49 +0400

    String should be partitioned based on parameter.

--- src/util/StringPartition.java
+++ src/util/StringPartition.java
@@ -4,11 +4,10 @@ public class StringPartition {
     public final String head;
     public final String tail;
         
-    public StringPartition(String string, int depth) {
-        head = string.substring(0, depth);
-        tail = (string.length() > depth) ?
-            string.substring(1, string.length()) :
-            new String();
+    public StringPartition(String string, int pivot) {
+        head = string.substring(0, pivot);
+        tail = (pivot > string.length()) ?
+            new String() : string.substring(pivot);
     }
 
     public StringPartition(String string) {
