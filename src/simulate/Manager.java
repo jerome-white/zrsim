def3bef348b4146c26f5487b94efeebb8492c321
Jerome White <jerome@thewex.net>
2017-06-20 14:38:34 +0400

    Remove Location; move Manager out of test.

--- src/simulate/Manager.java
+++ src/simulate/Manager.java
@@ -1,4 +1,4 @@
-package test.system;
+package simulate;
 
 import java.io.File;
 import java.io.PrintStream;
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.nio.channels.FileChannel;
 import java.lang.Long;
 import java.lang.IllegalStateException;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.concurrent.TimeUnit;
@@ -20,35 +21,27 @@ import java.util.concurrent.ConcurrentHashMap;
 import tree.SuffixTree;
 import tree.DocumentParser;
 import visitor.OutputVisitor;
-import visitor.SuffixTreeVisitor;
 import visitor.MarkRedundantVisitor;
 
 public class Manager {
     private final static Logger LOGGER =
         Logger.getLogger(Manager.class.getName());
 
-    public static void main(String[] args) {
-        LOGGER.setLevel(Level.INFO);
+    private SuffixTree suffixTree;
 
-        File directory = new File(args[0]);
-        int min_ngrams = Integer.parseInt(args[1]);
-        int max_ngrams = Integer.parseInt(args[2]);
-        File output = new File(args[3]);
-        File tmpdir = (args.length > 4) ? new File(args[4]) : null;
+    public Manager(int min_ngram) {
+        suffixTree = new SuffixTree(min_ngram);
+    }
 
-        SuffixTree root = new SuffixTree(min_ngrams);
-
-        /*
-         * add the documents to the suffix tree
-         */
+    public void addDocuments(File corpus, int max_ngram) {
         LOGGER.info("Adding terms");
 
         int pool = Runtime.getRuntime().availableProcessors();
         ExecutorService es = Executors.newFixedThreadPool(pool);
 
-        for (File document : directory.listFiles()) {
+        for (File document : corpus.listFiles()) {
             assert !document.isDirectory();
-            es.execute(new DocumentParser(root, document, max_ngrams));
+            es.execute(new DocumentParser(suffixTree, document, max_ngram));
         }
         es.shutdown();
 
@@ -58,28 +51,27 @@ public class Manager {
         catch (InterruptedException ex) {
             throw new IllegalStateException(ex);
         }
+    }
 
-        /*
-         * Prune the tree by marking entries that are redundant
-         */
+    public void selectTerms() {
         LOGGER.info("Term selection");
 
-        root.getChildren().forEach(1, (k, v) -> {
-                v.accept(new MarkRedundantVisitor(k, root));
+        suffixTree.getChildren().forEach(1, (k, v) -> {
+                v.accept(new MarkRedundantVisitor(k, suffixTree));
             });
+    }
 
-        /*
-         * Dump the tree to disk
-         */
-        LOGGER.info("Terms to disk");
+    public void generate(File output) {
+        String slurm = "SLURM_JOBTMP";
+        Map<String, String> env = System.getenv();
+        File tmpdir = env.containsKey(slurm) ? new File(env.get(slurm)) : null;
 
         ConcurrentHashMap<String, File> fragments =
             new ConcurrentHashMap<String, File>();
-        root.getChildren().forEach(1, (k, v) -> {
+        suffixTree.getChildren().forEachKey(1, k -> {
                 try {
                     File tmpfile = File.createTempFile(k, null, tmpdir);
                     tmpfile.deleteOnExit();
-
                     assert !fragments.containsKey(k);
                     File previous = fragments.put(k, tmpfile);
                 }
@@ -88,7 +80,9 @@ public class Manager {
                 }
             });
 
-        root.getChildren().forEach(1, (k, v) -> {
+        LOGGER.info("Terms to disk");
+
+        suffixTree.getChildren().forEach(1, (k, v) -> {
                 File file = fragments.get(k);
                 try (PrintStream out =
                      new PrintStream(new FileOutputStream(file), true)) {
@@ -111,6 +105,22 @@ public class Manager {
         catch (IOException ex) {
             throw new UncheckedIOException(ex);
         }
+    }
+
+    public static void main(String[] args) {
+        LOGGER.setLevel(Level.INFO);
+
+        File directory = new File(args[0]);
+        int min_ngram = Integer.parseInt(args[1]);
+        int max_ngram = Integer.parseInt(args[2]);
+        File output = new File(args[3]);
+
+        LOGGER.info("Begin");
+
+        Manager manager = new Manager(min_ngram);
+        manager.addDocuments(directory, max_ngram);
+        manager.selectTerms();
+        manager.generate(output);
 
         LOGGER.info("Complete");
     }
