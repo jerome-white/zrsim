fee548c2db75504149ae0c2638836973ddbaff0d
Jerome White <jerome@thewex.net>
2017-06-21 08:56:30 +0400

    Support for worker specification.

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 min_ngram=4
-while getopts "n:m:h" OPTION; do
+workers=20
+while getopts "n:m:w:h" OPTION; do
     case $OPTION in
 	n) min_ngram=$OPTARG ;;
 	m) max_ngram=$OPTARG ;;
+        w) workers=$OPTARG ;;
         h)
             exit
             ;;
@@ -42,19 +44,20 @@ tar \
     --file=$SCRATCH/zrt/corpus.tar.bz
 
 ant slurm \
-    -Dmin_ngrams=$min_ngram \
-    -Dmax_ngrams=$i \
+    -Dmin_ngram=$min_ngram \
+    -Dmax_ngram=$i \
     -Dcorpus=\$SLURM_JOBTMP/corpus \
-    -Doutput=$output
+    -Doutput=$output \
+    -Dworkers=$workers
 EOF
 
     sbatch \
 	--mem=240G \
-	--time=12:00:00 \
+	--time=24:00:00 \
 	--mail-type=END,FAIL \
 	--mail-user=jsw7@nyu.edu \
 	--nodes=1 \
-	--cpus-per-task=20 \
+	--cpus-per-task=$workers \
 	--workdir=$HOME/src/zrsim \
 	--job-name=zrsim-$i \
 	$job
