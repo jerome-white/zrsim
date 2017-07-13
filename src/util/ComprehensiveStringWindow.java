da27a41a7ad8f2dda69512938b8ca63b9236e0d0
Jerome White <jerome@thewex.net>
2017-07-13 11:58:22 +0400

    Support for minimum and maximum size specifications.

--- src/util/ComprehensiveStringWindow.java
+++ src/util/ComprehensiveStringWindow.java
@@ -5,20 +5,37 @@ import java.util.Iterator;
 public class ComprehensiveStringWindow extends StringWindow {
     private int left;
     private int right;
+    private int length;
+    private int minimum;
+    private int maximum;
     private boolean report_self;
 
     private String string;
 
-    public ComprehensiveStringWindow(String string, boolean report_self) {
+    public ComprehensiveStringWindow(String string,
+				     int minimum,
+				     int maximum,
+				     boolean report_self) {
         this.string = string;
+	this.minimum = minimum;
+	this.maximum = maximum;
         this.report_self = report_self;
 
         left = 0;
-        right = 1;
+        right = minimum;
+	length = string.length();
+    }
+
+    public ComprehensiveStringWindow(String string, int minimum, int maximum) {
+	this(string, minimum, maximum, false);
+    }
+
+    public ComprehensiveStringWindow(String string, int minimum) {
+	this(string, minimum, string.length());
     }
 
     public ComprehensiveStringWindow(String string) {
-        this(string, false);
+        this(string, 1);
     }
 
     public Iterator<String> iterator() {
@@ -26,19 +43,20 @@ public class ComprehensiveStringWindow extends StringWindow {
     }
 
     public boolean hasNext() {
-        return left < string.length();
+        return left + minimum <= length;
     }
 
     public String next() {
-        String s = string.substring(left, right);
+        String sub = string.substring(left, right);
 
         right++;
-        if (right == string.length() && left == 0 && !report_self ||
-            right > string.length()) {
+        if (right > length ||
+	    right - left > maximum ||
+	    right == length && left == 0 && !report_self) {
             left++;
-            right = left + 1;
+            right = left + minimum;
         }
 
-        return s;
+        return sub;
     }
 }
