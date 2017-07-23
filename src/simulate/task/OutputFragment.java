c5cdbec39fb39e1f47ae8b2de974ac3391539cf7
Jerome White <jerome@thewex.net>
2017-07-23 17:59:22 +0400

    Responsible for writing lots of ngrams to the same file.

--- src/simulate/task/OutputFragment.java
+++ src/simulate/task/OutputFragment.java
@@ -5,45 +5,57 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.concurrent.Callable;
 
 import tree.SuffixTree;
 import visitor.OutputVisitor;
 
 public class OutputFragment implements Callable<String> {
-    private Path path;
-    private SuffixTree root;
-    private String ngram;
     private int appearances;
     private boolean redundants;
 
-    public OutputFragment(Path path,
-			  SuffixTree root,
-			  String ngram,
+    private Path path;
+    private Path tmpfile;
+    private SuffixTree root;
+    private Collection<String> ngrams;
+
+    public OutputFragment(SuffixTree root,
+			  Collection<String> ngrams,
+			  Path tmpfile,
 			  int appearances,
 			  boolean redundants) {
-	this.path = path;
 	this.root = root;
-	this.ngram = ngram;
+	this.ngrams = ngrams;
+	this.tmpfile = tmpfile;
 	this.appearances = appearances;
 	this.redundants = redundants;
     }
 
-    public OutputFragment(Path path, SuffixTree root, String ngram) {
-	this(path, root, ngram, 2, false);
+    public OutputFragment(SuffixTree root,
+			  Collection<String> ngrams,
+			  Path tmpfile) {
+	this(root, ngrams, tmpfile, 2, false);
     }
 
     public String call() {
+	StringBuffer results = new StringBuffer();
+
 	try (PrintStream printStream =
-	     new PrintStream(Files.newOutputStream(path), true)) {
-	    OutputVisitor visitor =
-		new OutputVisitor(ngram, appearances, redundants, printStream);
-	    root.accept(visitor);
+	     new PrintStream(Files.newOutputStream(tmpfile), true)) {
+	    for (String n : ngrams) {
+		OutputVisitor visitor =
+		    new OutputVisitor(n, appearances, redundants, printStream);
+		root.accept(visitor);
+		results
+		    .append(n)
+		    .append(",");
+	    }
 	}
 	catch (IOException ex) {
 	    throw new UncheckedIOException(ex);
 	}
 
-	return ngram;
+	return results.toString();
     }
 }
