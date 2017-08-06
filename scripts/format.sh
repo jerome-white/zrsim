b41d632e558e40a87fed9e3a10fa62336c74217c
Jerome White <jerome@thewex.net>
2017-08-06 18:00:52 +0400

    Batch script to delete trailing whitespace and untabify.

---
+++ scripts/format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tmp=`mktemp`
+cat <<EOF > $tmp
+(defun emacs-format-function ()
+   "Format the whole buffer."
+   (c-set-style "java")
+   (indent-region (point-min) (point-max) nil)
+   (untabify (point-min) (point-max))
+   (delete-trailing-whitespace)
+   (save-buffer)
+)
+EOF
+
+for i in `find . -name '*.java'`; do
+    emacs -batch $i -l $tmp -f emacs-format-function
+done
+
+rm $tmp
