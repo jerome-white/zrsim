69c42d6cc69b58eec14d170342f9a1d009a7494d
Jerome White <jerome@thewex.net>
2017-08-07 05:14:13 -0400

    More robust to various run parameters.

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+#
+# defaults
+#
+workers=20
 min_ngram=4
-while getopts "n:m:h" OPTION; do
+memory=480
+duration=6:00:00
+
+while getopts "n:m:s:w:r:d:t:h" OPTION; do
     case $OPTION in
 	n) min_ngram=$OPTARG ;;
 	m) max_ngram=$OPTARG ;;
+	s) starting_ngram=$OPTARG ;;
+	w) workers=$OPTARG ;;
+	r) memory=$OPTARG ;;
+	d) duration=$OPTARG ;;
+	t) trees=$OPTARG ;; # trees=$SCRATCH/zrt/wsj/2017_0719_184233/trees
         h)
             exit
             ;;
@@ -12,16 +24,19 @@ while getopts "n:m:h" OPTION; do
     esac
 done
 
-module try-load jdk
-module try-load apache-ant
+module try-load jdk/1.8.0_111
+module try-load apache-ant/1.9.8
 ant clean compile || exit 1
 
-trees=$SCRATCH/zrt/wsj/2017_0615_175330/trees
 # rm --recursive --force $trees
 # mkdir $trees
 rm --force slurm-*
 
-for i in `seq $min_ngram $max_ngram`; do
+if [ ! $starting_ngram ]; then
+    starting_ngram=$min_ngram
+fi
+
+for i in `seq $starting_ngram $max_ngram`; do
     output=$trees/`printf "%02.f" $i`.csv
     if [ -e $output ]; then
 	continue
@@ -31,9 +46,6 @@ for i in `seq $min_ngram $max_ngram`; do
 
     cat <<EOF > $job
 #!/bin/bash
-
-# module try-load jdk
-# module try-load apache-ant
 
 tar \
     --extract \
@@ -46,16 +58,17 @@ ant slurm \
     -Dmax_ngram=$i \
     -Dcorpus=\$SLURM_JOBTMP/corpus \
     -Doutput=$output
+    -Dworkers=$workers
 EOF
 
     sbatch \
-	--mem=240G \
-	--time=24:00:00 \
+	--mem=${memory}G \
+	--time=$duration \
 	--mail-type=END,FAIL \
 	--mail-user=jsw7@nyu.edu \
 	--nodes=1 \
-	--cpus-per-task=20 \
-	--workdir=$HOME/src/zrsim \
-	--job-name=zrsim-$i \
+	--cpus-per-task=$workers \
+	--workdir=`pwd` \
+	--job-name=zrcomp-$i \
 	$job
 done > jobs
