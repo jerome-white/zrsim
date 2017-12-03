d66a67d4bcddf0943bc02dd9bbcd4b29d23e0068
Jerome White <jerome@thewex.net>
2017-12-03 20:42:46 +0400

    Responsible for creating Soundex instance.

--- src/util/transform/SoundexTransformer.java
+++ src/util/transform/SoundexTransformer.java
@@ -2,16 +2,8 @@ package util.transform;
 
 import org.apache.commons.codec.language.Soundex;
 
-public class SoundexTransformer extends TransformDecorator {
-    Soundex soundex;
-
+public class SoundexTransformer extends ApacheCodecTransformer {
     public SoundexTransformer(NgramTransformer transformer) {
-        super(transformer);
-
-        soundex = new Soundex();
-    }
-
-    public String transform(String ngram) {
-        return super.transform(soundex.encode(ngram));
+        super(transformer, new Soundex());
     }
 }
